1fbbe012ed5f190ce7914fc6813509eb28721cb3
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-11-24 16:48:11 +0100

    uppdated mockdata

--- lib/mockData.ts
+++ lib/mockData.ts
@@ -1,9 +1,10 @@
 // /lib/mockData.ts
 
 export const representatives = [
-    { id: 1, name: 'Andrea', email: 'andrea@example.com', publicVotes: 10 },
-    { id: 2, name: 'Beatrice', email: 'beatrice@example.com', publicVotes: 20 },
-  ];
+  { id: 1, name: 'Andrea', email: 'andrea@example.com', publicVotes: 10, publicPreferences: { 'GB Glace': 5, 'Sia Glass': 5 } },
+  { id: 2, name: 'Beatrice', email: 'beatrice@example.com', publicVotes: 20, publicPreferences: { 'GB Glace': 15, 'Triumf Glass': 5 } },
+];
+
   
   export const elections = [
     {
