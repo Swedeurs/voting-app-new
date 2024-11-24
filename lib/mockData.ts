216a86397e29e9aa7d2aba792c14d409b25efc43
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-11-24 16:25:34 +0100

    uppdated mock data

--- lib/mockData.ts
+++ lib/mockData.ts
@@ -1,3 +1,5 @@
+// /lib/mockData.ts
+
 export const representatives = [
     { id: 1, name: 'Andrea', email: 'andrea@example.com', publicVotes: 10 },
     { id: 2, name: 'Beatrice', email: 'beatrice@example.com', publicVotes: 20 },
@@ -10,6 +12,12 @@ export const representatives = [
       choices: ['GB Glace', 'Sia Glass', 'Hemglass', 'Triumf Glass'],
       status: 'ongoing',
       votes: {
+        'GB Glace': 0,
+        'Sia Glass': 0,
+        'Hemglass': 0,
+        'Triumf Glass': 0,
+      },
+      publicPreferences: {
         'GB Glace': 0,
         'Sia Glass': 0,
         'Hemglass': 0,
