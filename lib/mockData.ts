171e1afbf09b53a60de5f119867b80fd7f0c93d8
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-11-24 16:04:07 +0100

    initailized and made mockdata

---
+++ lib/mockData.ts
@@ -0,0 +1,20 @@
+export const representatives = [
+    { id: 1, name: 'Andrea', email: 'andrea@example.com', publicVotes: 10 },
+    { id: 2, name: 'Beatrice', email: 'beatrice@example.com', publicVotes: 20 },
+  ];
+  
+  export const elections = [
+    {
+      id: 1,
+      name: 'Best Swedish Ice Cream',
+      choices: ['GB Glace', 'Sia Glass', 'Hemglass', 'Triumf Glass'],
+      status: 'ongoing',
+      votes: {
+        'GB Glace': 0,
+        'Sia Glass': 0,
+        'Hemglass': 0,
+        'Triumf Glass': 0,
+      },
+    },
+  ];
+  
