4e58dfdfdf2893414db70f9449774bff7b38df47
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-11-24 18:36:04 +0100

    modularized hooks

---
+++ src/hooks/use-elections.ts
@@ -0,0 +1,12 @@
+import useSWR from 'swr';
+
+const fetcher = (url: string) => fetch(url).then(res => res.json());
+
+export default function useElections() {
+  const { data, error } = useSWR('/api/elections', fetcher);
+  return {
+    elections: data,
+    isLoading: !error && !data,
+    isError: error,
+  };
+}
