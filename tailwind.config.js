0c200e54a926aa5f9b6b49a491230834e9c2ff5f
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-11-24 19:57:20 +0100

    prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',  // Adjust these paths to point to your source directory
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/features/**/*.{js,ts,jsx,tsx}', // If you have features directory
+    "./src/pages/**/*.{js,ts,jsx,tsx}", // Adjust these paths to point to your source directory
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/features/**/*.{js,ts,jsx,tsx}", // If you have features directory
   ],
   theme: {
     extend: {},
